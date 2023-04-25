4208e7741355ebe636677066051b0715c66b16de
Kehyan88 <kehyan88@gmail.com>
2023-04-26 00:02:48 +0400

    Упражнение 183

--- Mikayel.py
+++ Mikayel.py
@@ -1,2 +1,18 @@
-print('test')
-print('test1')
+# Упражнение 183. Последовательность химических
+# элементов
+name = input('Մուտքագրեք քիմիական տարր: ')
+new_lst = []
+def game(name):
+    lst =  ['hydrogen','nitrogen','nickel','lanthanum','magnesium','beryllium','mendelevium']
+    if name not in lst:
+        return 'Այդպիսի քիմիական տարր գոյություն չունի\nԴՈՒՔ ՊԱՐՏՎԵՑԻՔ'
+    elif name in lst:
+        new_lst.append(name)
+        name2 = input("Մուտքագրեք քիմիական տարր: ")
+        if name[len(name) - 1] == name2[0] and name2 not in new_lst:
+            name = name2
+            return game(name)
+        return 'ԴՈՒՔ ՊԱՐՏՎԵՑԻՔ'
+
+
+print(game(name))
