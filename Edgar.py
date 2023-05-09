5d4ce20ac7b41ee21018d032890a45708095c6eb
edgar021987 <edgar302015@gmail.com>
2023-05-09 21:41:06 +0400

    arm

--- Edgar.py
+++ Edgar.py
@@ -1,6 +1,31 @@
-num = int(input('Mutqagreq tuv: '))
-lst = []
-while num != 0:
-    lst.append(num)
-    num = int(input('Mutqagreq tuv: '))
-print(lst)
+# List
+# num = int(input('Mutqagreq tuv: '))
+# lst = []
+# while num != 0:
+#     lst.append(num)
+#     num = int(input('Mutqagreq tuv: '))
+# print(lst)
+
+# lst1 = ['a','b','c','d']
+# lst2 = [12,56,85,65]
+# dct = {}
+# for i in range(0, len(lst1)):
+#     dct[lst1[i]] = lst2[i]
+# print(dct)
+
+# from random import randint
+
+# dct = {'a':1,'b':2,'c':3,'d':4}
+# lst = []
+# for i in dct:
+#     lst.append(i)
+# del dct[lst[randint(0,len(lst)-1)]]
+# print(dct)
+
+# dct = {'c':3,'a':4,'b':1,'d':2}
+# dct1 = dict(sorted(dct.items()))
+# print(dct1)
+# dct3 = dict(sorted(dct.items(), key = lambda item: item[1]))
+# print(dct3)
+
+
