e42f67f42b3fb2edee51d6dd28de04154f0e1397
edgar021987 <edgar302015@gmail.com>
2023-05-22 15:31:31 +0400

    das

--- Edgar.py
+++ Edgar.py
@@ -64,3 +64,111 @@
 #     return new_x 
 
 print(lst(x))
+
+
+# try:
+#     nshan = input('texadrel + - / *: ' ) 
+#     x = float(input('texadrel tiv1: '))
+#     y = float(input('texadrel tiv2: '))
+# except (ValueError, TypeError):
+#     print('Sxal mutqagrum')
+# if nshan == '+':
+#     print(x + y)
+# elif nshan == '-':
+#     print(x - y)
+# elif nshan == '/':
+#     print(x / y)    
+# elif nshan == '*':
+#     print(x * y)
+    
+    
+
+# def pasvord_genaration(passvord):
+#     for i in passvord:
+#      if i  == :
+#         print('tuyl passvord')
+#      elif i == string.ascii_letters + string.digits:
+#         print('migin passvord')
+#      elif i == string.ascii_lowercase +string.ascii_letters + string.digits:
+#         print('ujex passvord')
+# print(pasvord_genaration(12355678910))        
+                  
+# import requests
+# import json
+# url = 'https://jsonplaceholder.typicode.com/posts'
+# response = requests.get(url)
+# data = json.loads(response.text)
+# for i in data:
+#  print( i.keys(), i. )
+
+
+# def binary_search(lst, x):
+#     mid = len(lst)//2 
+#     left = lst[(len(lst)-1 //2)-1]
+#     right = lst[len(lst)//2]
+#     if lst[mid] == x or left == x or right == x:
+#         return x
+#     elif mid == 0:
+#         return 'argeq@ bacakayum e'
+#     elif x < left:
+#         return search:
+
+# class Mard:
+#     def __init__(self , tariq, amis, cnndyan_or):
+#         self.tariq = tariq
+#         self.amis = amis
+#         self.cnndyan_or = cnndyan_or
+
+# user1 = Mard(20, 6, 22)
+# print(user1.tariq)
+
+    
+# class Mynumbers:
+#     def __iter__(self):
+#         self.a = 1
+#         return self
+#     def __next__(self):
+#         x = self.a
+#         x += 1
+#         return x
+    
+#class BankAccount:
+    # db = []
+    # def __init__ (self, fullname, account_number, balance):
+    #     self.name = fullname
+    #     self.num = account_number
+    #     self.balance = balance
+        
+    # def deposit(self, amount):
+    #     self.balance += amount
+    #     self.db.append(f'Deposit | {})
+            
+# class Lst:
+#     def __init__(self, lst):
+#         self.lst = lst
+#     def max(self):
+#         m = self.Lst([0])
+#         for i in self.lst:
+#           if m < i:
+#               m = i
+#     def min(self):
+#         m = self.lst([0])
+#         for i in self.lst:
+#             if m > i:
+#                 m = i
+# m =                 
+# print(m.max)                            
+   
+# class Mard:
+#     def __init__(self , tariq, amis, cnndyan_or):
+#         self.tariq = tariq
+#         self.amis = amis
+#         self.cnndyan_or = cnndyan_or
+# class Mard_jarangutyun(Mard):
+#     def __init__(self, tariq, amis, cnndyan_or):
+#         super().__init__(tariq, amis, cnndyan_or)
+
+# user1 = Mard(20, 6, 22)
+# print(user1.tariq)
+
+
